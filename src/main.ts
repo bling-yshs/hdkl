2768f4ef168aac1be29b149844a7dfd710df458f
bling-yshs <377178599@qq.com>
2025-05-01 10:13:15 +0800

    feat: env支持双引号2

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue'
-// main.ts
 import 'virtual:uno.css'
+const pinia = createPinia()
+import { createPinia } from 'pinia'
 const app = createApp(App)
 app.use(router)
 app.use(Antd)
+app.use(pinia)
 app.mount('#app')
